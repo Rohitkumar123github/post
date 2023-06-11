8f3c5c8f0dae082361a01a5f409e976359f33d1c
Rohitkumar123github <rfaltu77@gmail.com>
2023-06-11 16:02:52 +0530

    darkmode and lightmode implemented- stable

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
-
+import styles from './page.module.css'
+import Image from 'next/image'
 const BlogPost  = () => {
   return (
-    <div>BlogPost</div>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos eaque, in rem alias dolor, a cupiditate et itaque error velit totam neque! Reprehenderit ex tempora nam ipsam. Sequi, sunt enim!</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur obcaecati, consectetur neque totam libero debitis magni dolor eligendi quas, nemo cupiditate error non quia sunt voluptatum ad aspernatur natus ullam?
+            Sapiente fuga voluptas iusto molestiae unde ipsum vitae maiores doloribus. Dicta magnam quisquam ipsa recusandae quam ea reprehenderit, tenetur cum earum dolorum. Est dolorum quae sint dignissimos porro eaque perspiciatis!
+            Cum magni ipsum veritatis porro eveniet, sequi temporibus itaque harum, hic voluptatum cupiditate natus! Sint nemo ullam doloremque maiores numquam blanditiis ipsum laboriosam officia iusto quis quasi incidunt, harum minus?
+            Pariatur praesentium aspernatur laborum, sapiente quidem doloribus odio provident mollitia, eveniet reiciendis ea eius. In, odio consequuntur odit et quia sed deleniti consectetur molestiae eligendi fuga aperiam non dolores ab?
+            Voluptatibus error officiis eligendi optio, maxime saepe nemo totam facere quod, ab ex facilis debitis provident itaque veniam soluta placeat impedit maiores commodi asperiores a? Soluta velit sint suscipit beatae.
+          </p>
+          <div className={styles.author}>
+            <Image
+              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Z2lybHN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+              alt=''
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>Potaatoo</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.unsplash.com/photo-1548372157-5287da32e872?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzF8fGdpcmxzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+            alt=''
+            fill={true}
+            className={styles.image}
+          />    
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+          paragaraph
+        </p>
+      </div>
+    </div>
   )
 }
 
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
 import styles from './page.module.css'
-const Category = () => {
+import Button from '../../../components/Button/Button'
+import Image from 'next/image'
+const Category = ({params}) => {
+  console.log(params)
   return (
-    <div>Category</div>
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1>Test1a</h1>
+          <p>Desc</p>
+          <Button url="#" text="See More"/>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image 
+          src="https://plus.unsplash.com/premium_photo-1679619557768-03c2ce276555?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+          fill={true}
+          alt=""
+          className={styles.img}
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1>Test2</h1>
+          <p>Desc</p>
+          <Button url="#" text="See More"/>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image 
+          src="https://plus.unsplash.com/premium_photo-1679619557768-03c2ce276555?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+          fill={true}
+          alt=""
+          className={styles.img}
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1>Test</h1>
+          <p>Desc</p>
+          <Button url="#" text="See More"/>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image 
+          src="https://plus.unsplash.com/premium_photo-1679619557768-03c2ce276555?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+          fill={true}
+          alt=""
+          className={styles.img}
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
