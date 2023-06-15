69e97c09513742e9f61f06dee09b841981bece14
Rohitkumar123github <rfaltu77@gmail.com>
2023-06-16 00:05:57 +0530

    added protected routes functionality into dashboard

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,9 +1,12 @@
 "use client"
 import React from 'react'
 import styles from './page.module.css'
-import { signIn } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
+import { useRouter} from "next/navigation"
 const Login = () => {
 
+  const session = useSession()
+  const router = useRouter()
   const handleSubmit=async(e)=>{
     e.preventDefault()
 
@@ -12,16 +15,28 @@ const Login = () => {
 
     signIn("credentials", {email, password })
   }
-  return (
-    <div className={styles.container}>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <input type='email' placeholder='email' className={styles.input} required/>
-        <input type='password' placeholder='password' className={styles.input} required/>
-        <button className={styles.button}>Login</button>
-      </form>
-      <button onClick={()=>signIn("google")}>Login with Google</button>
-    </div>
-  )
+
+  if(session.status === "loading"){
+    return <p>Loading...</p>
+  }
+
+  if(session.status === "authenticated"){
+    router?.push("/dashboard")
+  }
+  if(session.status === "unauthenticated"){
+    return (
+      <div className={styles.container}>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <input type='email' placeholder='email' className={styles.input} required/>
+          <input type='password' placeholder='password' className={styles.input} required/>
+          <button className={styles.button}>Login</button>
+        </form>
+        <button onClick={()=>signIn("google")}>Login with Google</button>
+      </div>
+    )
+  }
+
+  
 }
 
 export default Login
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,6 +3,7 @@ import React,{useState, useEffect} from 'react'
 import styles from './page.module.css'
 import useSWR from 'swr'
 import { useSession, signIn, signOut } from "next-auth/react"
+import {useRouter} from "next/navigation"
 
 
 const Dashboard = () => {
@@ -33,15 +34,29 @@ const Dashboard = () => {
 
   const session = useSession()
   console.log(session)
-  //fetching data using SWR 
+
+  const router = useRouter()
+
+    //fetching data using SWR 
   //take account of isLoading, error and also we can mutate our data 
   const fetcher = (...args) => fetch(...args).then(res => res.json())
   const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
+
+  if(session.status === "loading"){
+    return <p>Loading...</p>
+  }
+  if(session.status === "unauthenticated"){
+    router?.push("/dashboard/login")
+  }
+
   // console.log(data)
   // console.log(isLoading)
-  return (
-    <div>Dashboard</div>
-  )
+  if(session.status === "authenticated"){
+    return (
+      <div className={styles.container}>Dashboard</div>
+    )
+  }
+  
 }
 
 export default Dashboard
