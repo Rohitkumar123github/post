fa4a291edd5b2a775dbb4ab82d3fccb6fcffd395
Rohitkumar123github <rfaltu77@gmail.com>
2023-06-15 22:36:11 +0530

    added nextAuth and provided google login option

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,6 +2,7 @@
 import React,{useState, useEffect} from 'react'
 import styles from './page.module.css'
 import useSWR from 'swr'
+import { useSession, signIn, signOut } from "next-auth/react"
 
 
 const Dashboard = () => {
@@ -30,12 +31,14 @@ const Dashboard = () => {
   //   getData()
   // }, [])
 
+  const session = useSession()
+  console.log(session)
   //fetching data using SWR 
   //take account of isLoading, error and also we can mutate our data 
   const fetcher = (...args) => fetch(...args).then(res => res.json())
   const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
-  console.log(data)
-  console.log(isLoading)
+  // console.log(data)
+  // console.log(isLoading)
   return (
     <div>Dashboard</div>
   )
